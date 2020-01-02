21c1497216fa02b81aaf368f44a3e36c06eeaa0b
KlewTheMystic <59288443+KlewTheMystic@users.noreply.github.com>
2020-01-02 15:35:21 +0100

    Update portfolio.js

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -42,7 +42,7 @@ GitHub: https://github.com/Lumm1t/obnoxious.club
         var links = [
             {
                 name: 'Mateyo',
-                link: '76561198977534578'
+                link: '76561199010250248'
             },
                 
             {
